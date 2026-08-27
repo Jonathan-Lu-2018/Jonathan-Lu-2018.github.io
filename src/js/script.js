befd42a6db90870f5bda4153ddbb92dd7df76508
Jonathan Lu <jonathanhlu2022@gmail.com>
2026-08-26 22:24:24 -0700

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -143,17 +143,21 @@ const pages = document.querySelectorAll("[data-page]");
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
+    const targetPage = this.innerText.trim().toLowerCase();
 
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
+    for (let pageIndex = 0; pageIndex < pages.length; pageIndex++) {
+      if (targetPage === pages[pageIndex].dataset.page) {
+        pages[pageIndex].classList.add("active");
         window.scrollTo(0, 0);
       } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
+        pages[pageIndex].classList.remove("active");
       }
     }
 
+    for (let linkIndex = 0; linkIndex < navigationLinks.length; linkIndex++) {
+      navigationLinks[linkIndex].classList.remove("active");
+    }
+
+    this.classList.add("active");
   });
 }
